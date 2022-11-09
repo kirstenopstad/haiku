56fe381095f21ef79f96761a574b6a3da1bb836d
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-09 11:19:02 -0800

    passes test to return vowel count for checkWord
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>

--- src/js/haiku-checker.js
+++ src/js/haiku-checker.js
@@ -15,10 +15,30 @@ export class Haiku {
         return "too many lines";
       }else if (lineArray.length < 3) {
         return "not enough lines";
+      } else {
+        //syllable checking
+        this.line1 = lineArray[0];
+        this.line2 = lineArray[1];
+        this.line3 = lineArray[3];
       }
+      
       return "poem";
     } else {
       return "not a poem";
     }
+  }
+
+  checkWord = (word) => {
+    // define vowel set
+    const vowels = /[aeiou]/
+    let vowelCount = 0;
+    for (let i = 0; i < word.length; i++) {
+      if (vowels.test(word[i])) {
+        console.log(word[i]);
+        vowelCount++;
+      }
+    }
+    return vowelCount;
+    
   }
 }
