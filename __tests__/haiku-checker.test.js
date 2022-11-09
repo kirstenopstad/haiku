92a70e2e37d2f564727fa3c3232f9fa65c12f52c
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-09 09:54:14 -0800

    add test 'It should return 'not a poem' if no text is inputted'
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>

--- __tests__/haiku-checker.test.js
+++ __tests__/haiku-checker.test.js
@@ -1,5 +1,19 @@
-import { checkHaiku } from '../src/js/haiku-checker';
+import { checkHaiku } from '../src/js/haiku-checker.js';
+import { Haiku } from '../src/js/haiku-checker.js';
 
-describe('It should return "poem" if text is inputted', () => {
-  expect(checkHaiku("this is a pipe")).toEqual("poem");
+describe('check haiku', () => {
+  let haiku;
+
+  beforeEach(() => {
+    haiku = new Haiku();
+  });
+
+  test('It should return "not a poem" if no text is inputted', () => {
+    expect(checkHaiku()).toEqual("not a poem");
+  });
+
+  test('It should return "poem" if text is inputted', () => {
+    expect(checkHaiku("this is a pipe")).toEqual("poem");
+  });
+  
 })
